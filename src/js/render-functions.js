9539a61a6946bcd2955545871c6763b156a0f868
dimondesh <dimondesh1337@gmail.com>
2024-11-18 21:35:34 +0200

    added stats for images

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,11 +1,27 @@
 export function renderGallery(images) {
   return images
-    .map(({ webformatURL, largeImageURL, tags }) => {
-      return `
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
       <a href="${largeImageURL}" class="gallery-item">
         <img src="${webformatURL}" alt="${tags}" />
+        <div class="info">
+          <p class="info-item"><b>Likes:</b> ${likes}</p>
+          <p class="info-item"><b>Views:</b> ${views}</p>
+          <p class="info-item"><b>Comments:</b> ${comments}</p>
+          <p class="info-item"><b>Downloads:</b> ${downloads}</p>
+        </div>
       </a>
     `;
-    })
+      }
+    )
     .join('');
 }
